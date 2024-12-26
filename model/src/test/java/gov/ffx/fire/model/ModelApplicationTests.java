e60989a8286c5afec645c8c319d9d2aa89fb85d7
Louis <louis.drotos@gmail.com>
2024-12-26 17:41:24 -0500

    Created projects

---
+++ model/src/test/java/gov/ffx/fire/model/ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.ffx.fire.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
